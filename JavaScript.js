64b8369a233c9dd75e53e550658a6aa07c768c7a
Pedro Henrique Cardoso Monteiro <91520452+SrGinko@users.noreply.github.com>
2023-06-24 18:18:52 -0300

    v3
    codigo em javaScript

---
+++ JavaScript.js
@@ -0,0 +1,144 @@
+
+function A(){
+	let button = document.getElementById('name');
+	button.style.background = "red"
+	let button1 = document.getElementById('name2')
+	button1.style.background = "green"
+}
+function B(){
+	let button = document.getElementById('name1')
+	button.style.background = "red"
+	let button1 = document.getElementById('name2')
+	button1.style.background = "green"
+}
+function C(){
+	let button1 = document.getElementById('name2')
+	button1.style.background = "green"
+}
+function D(){
+	let button = document.getElementById('name3');
+	button.style.background = "red"
+	let button1 = document.getElementById('name2')
+	button1.style.background = "green"
+}
+
+
+function A1(){
+	let button = document.getElementById('name4');
+	button.style.background = "red"
+	let button1 = document.getElementById('name6')
+	button1.style.background = "green"
+}
+function B1(){
+	let button = document.getElementById('name5')
+	button.style.background = "red"
+	let button1 = document.getElementById('name6')
+	button1.style.background = "green"
+}
+function C1(){
+	let button1 = document.getElementById('name6')
+	button1.style.background = "green"
+}
+function D1(){
+	let button = document.getElementById('name7');
+	button.style.background = "red"
+	let button1 = document.getElementById('name6')
+	button1.style.background = "green"
+}
+
+
+function A2(){
+	let button = document.getElementById('name8');
+	button.style.background = "red"
+	let button1 = document.getElementById('name10')
+	button1.style.background = "green"
+}
+function B2(){
+	let button = document.getElementById('name9')
+	button.style.background = "red"
+	let button1 = document.getElementById('name10')
+	button1.style.background = "green"
+}
+function C2(){
+	let button1 = document.getElementById('name10')
+	button1.style.background = "green"
+}
+function D2(){
+	let button = document.getElementById('name11');
+	button.style.background = "red"
+	let button1 = document.getElementById('name10')
+	button1.style.background = "green"
+}
+
+
+function A3(){
+	let button1 = document.getElementById('name12')
+	button1.style.background = "green"
+}
+function B3(){
+	let button = document.getElementById('name13')
+	button.style.background = "red"
+	let button1 = document.getElementById('name12')
+	button1.style.background = "green"
+}
+function C3(){
+	let button = document.getElementById('name14');
+	button.style.background = "red"
+	let button1 = document.getElementById('name12')
+	button1.style.background = "green"
+}
+function D3(){
+	let button = document.getElementById('name15');
+	button.style.background = "red"
+	let button1 = document.getElementById('name12')
+	button1.style.background = "green"
+}
+
+
+function A4(){
+	let button = document.getElementById('name16')
+	button.style.background = "red"
+	let button1 = document.getElementById('name17')
+	button1.style.background = "green"
+}
+function B4(){
+	let button1 = document.getElementById('name17')
+	button1.style.background = "green"
+}
+function C4(){
+	let button = document.getElementById('name18');
+	button.style.background = "red"
+	let button1 = document.getElementById('name17')
+	button1.style.background = "green"
+}
+function D4(){
+	let button = document.getElementById('name19');
+	button.style.background = "red"
+	let button1 = document.getElementById('name17')
+	button1.style.background = "green"
+}
+
+
+function A5(){
+	let button1 = document.getElementById('name20')
+	button1.style.background = "green"
+}
+function B5(){
+	let button = document.getElementById('name21');
+	button.style.background = "red"
+	let button1 = document.getElementById('name20')
+	button1.style.background = "green"
+}
+function C5(){
+	let button = document.getElementById('name22');
+	button.style.background = "red"
+	let button1 = document.getElementById('name20')
+	button1.style.background = "green"
+}
+function D5(){
+	let button = document.getElementById('name23');
+	button.style.background = "red"
+	let button1 = document.getElementById('name20')
+	button1.style.background = "green"
+}
+
